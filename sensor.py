ee61a934061996da30e096f3e7947cfe64bbf939
rebeccaYMCG <A00241382@mycambrian.ca>
2024-03-25 22:06:03 -0400

    Updated motionSensor Function to detect 10 seconds

--- sensor.py
+++ sensor.py
@@ -1,11 +1,12 @@
 from gpiozero import MotionSensor, Button
-from datetime import datetime
+from datetime import datetime, timedelta
 from time import sleep 
 import subprocess
 
 motionSensor = MotionSensor(23) # these will be changed for actual GPIO pins
 button = Button(13)
 
+# dates captured images
 def captureImage():
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     imagePath = f"motion_capture_{timestamp}.jpg"
@@ -16,18 +17,35 @@ def captureImage():
     # calls the face detection py
     subprocess.run(['python', 'faceDetection.py', imagePath])
 
+# initialize variables
+motion_start_time = None
+
+
+# function for checking if motion has been detected or button is pushed
 try: 
     print("Motion detection system is active. Press Crtl+C to exit.")
 
+    # function will now only take a photo if individual is within the sensor for more than 10 seconds
     while True:
         if motionSensor.motion_detected:
-            print("Motion Detected!")
-            captureImage()
+            if motion_start_time is None:
+                motion_start_time = datetime.now()
+                print("Motion Detected!")
+            else:
+                if datetime.now() - motion_start_time > timedelta(seconds=10):
+                    print("Motion Detected for more than 10 secounds!")
+                    captureImage()
+                    motion_start_time = None
 
+        else: 
+            motion_start_time = None
+
+        # when button is pressed it will take a photo, then sleep
         if button.is_pressed: 
             print("Button Pressed!")
             captureImage()
-    sleep(2)
+        
+        sleep(10)
 
 except KeyboardInterrupt:
     print("Motion detection system stopped.") 
